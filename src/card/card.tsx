d19095924900fb95e41f87e3e91feeb32ae00688
Joseba Ibarlucea <Jibarlucea91@gmail.com>
2023-04-07 11:21:23 +0200

    Add card draggable with props structure

--- src/card/card.tsx
+++ src/card/card.tsx
@@ -1,67 +1,26 @@
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
-import { Students } from "../list";
-import "./card.scss";
+import { Draggable } from 'react-beautiful-dnd';
+import './card.scss';
+import { Student } from '../models/students';
 
-export function Card() {
-  const studentsList = Students.sort((a, b) => 0.5 - Math.random());
-  const half = Math.ceil(studentsList.length / 2);
-  const firstHalf = studentsList.slice(0, half);
-  const secondHalf = studentsList.slice(half);
-
+export function Card(props: { students: Student[]; section: string }) {
   return (
     <>
-      <DragDropContext onDragEnd={() => {}}>
-        <Droppable droppableId="students">
+      {props.students.map((student, index) => (
+        <Draggable key={student.name} draggableId={student.name} index={index}>
           {(provided) => (
-            <div {...provided.droppableProps} ref={provided.innerRef}>
-              <ul className="container1">
-                {firstHalf.map((student, index) => (
-                  <Draggable
-                    key={student.name}
-                    draggableId={student.name}
-                    index={index}
-                  >
-                    {(provided) => (
-                      <li
-                        key={student.name}
-                        className="leftContainer"
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                      >
-                        <img src={student.img} alt={student.name} />
-                        <h2>{student.name}</h2>
-                      </li>
-                    )}
-                  </Draggable>
-                ))}
-              </ul>
-              <ul className="container2">
-                {secondHalf.map((student, index) => (
-                  <Draggable
-                    key={student.name}
-                    draggableId={student.name}
-                    index={index}
-                  >
-                    {(provided) => (
-                      <li
-                        key={student.name}
-                        className="rightContainer"
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                      >
-                        <img src={student.img} alt={student.name} />
-                        <h2>{student.name}</h2>
-                      </li>
-                    )}
-                  </Draggable>
-                ))}
-              </ul>
-            </div>
+            <li
+              key={student.name}
+              className={props.section}
+              ref={provided.innerRef}
+              {...provided.draggableProps}
+              {...provided.dragHandleProps}
+            >
+              <img src={student.img} alt={student.name} />
+              <h2>{student.name}</h2>
+            </li>
           )}
-        </Droppable>
-      </DragDropContext>
+        </Draggable>
+      ))}
     </>
   );
 }
